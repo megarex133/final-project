2d360e551c67348e520da677d46ca69229ca9b94
megarex133 <binby133@gmail.com>
2024-01-04 14:04:16 +0300

    Testimonials update

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,13 +1,16 @@
 import './styles/Testimonials.css';
 import Review from './Review';
-import manPhoto_1 from '../assets/man_1.jpg';
+import reviewPhoto from '../assets/review-person.jpg';
 
 function Testimonials() {
     return (
         <div className='testimonials-container'>
             <h2>Testimonials</h2>
             <div className='reviews-container'>
-                <Review stars='3' image={manPhoto_1} alt='man on the photo' name='John' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non eros placerat, vestibulum nibh vel, viverra elit. Donec consectetur velit a massa finibus, hendrerit pharetra nisi euismod. Vivamus pellentesque libero diam, a tincidunt turpis suscipit a.'/>
+                <Review stars='3' image={reviewPhoto} alt='man on the photo' name='John' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non eros placerat, vestibulum nibh vel, viverra elit.' />
+                <Review stars='5' image={reviewPhoto} alt='man on the photo' name='John' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non eros placerat, vestibulum nibh vel, viverra elit.' />
+                <Review stars='2' image={reviewPhoto} alt='man on the photo' name='John' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non eros placerat, vestibulum nibh vel, viverra elit.' />
+                <Review stars='4' image={reviewPhoto} alt='man on the photo' name='John' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non eros placerat, vestibulum nibh vel, viverra elit.'/>
             </div>
         </div>
     );
